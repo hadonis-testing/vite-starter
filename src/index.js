fef2f936472a791bc425c75bf88de33298a2af18
Minh Vương <hardingadonis@gmail.com>
2024-02-17 17:55:02 +0700

    refactor: :zap: finish video 9

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-console.log('hello from index.js');
+import image from './apple-touch-icon.png';
+
+const img = document.createElement('img');
+img.src = image;
+
+document.querySelector('#content').appendChild(img);
 
 // import { initializeCounter } from './counter';
 
